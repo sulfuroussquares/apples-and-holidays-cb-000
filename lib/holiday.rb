0f5f9341a110c9ddec675b3a3acb2bad2ed74379
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-25 03:33:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,54 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
+
+  holiday_hash.each do |key, value|
+    if (key == :winter)
+      value.each do |data, attribute|
+      attribute.push(supply)
+      end #end inner loop
+    end #endif
+  end
 
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
+holiday_hash.each do |key, value|
+  value.each do |data, attribute|
+    if (data == :memorial_day)
+      attribute.push(supply)
+    end #endif
+  end #end inner loop
+end #end outer loop
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+holiday_hash.each do |key, value|
+  if (key == season)
+    holiday_hash[season][holiday_name] = supply_array
+  end #endif
+end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter_supplies = []
+holiday_hash.each do |key, value|
+  if (key == :winter)
+    value.each do |data, attribute|
+      winter_supplies.push(attribute)
+    end # end inner loop
+  end #endif
+end #end outer loop
+return winter_supplies.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,7 +76,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |key, value|
+  puts "#{key.capitalize}:"
+    value.each do |data, attribute|
+      puts "  #{data.capitalize.to_s.gsub("_"," ")}: #{attribute.join(", ")}"
+    end #end inner loop
+end #end outer loop
 end
 
 def all_holidays_with_bbq(holiday_hash)
@@ -61,10 +89,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
